eddb2faa0758ba272ff78960292cc2d01e255e3a
Kevin Wakefield <kwakefield322@gmail.com>
2016-05-12 20:43:41 -0400

    Fourth Commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,12 @@
 class Book < ActiveRecord::Base
   #Each book belongs to a library
   belongs_to :library
+
+  #ensures that all fields are filled in
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :genre, presence: true
+  validates :publisher, presence: true
+  validates :date, presence: true
+  validates :location, presence: true
 end
--- app/models/library.rb
+++ app/models/library.rb
@@ -1,8 +1,12 @@
 #Generated from scaffolding
+#Modified
 class Library < ActiveRecord::Base
   #Each library belongs to a user
   belongs_to :user
 
   #Each library has many books
   has_many :books
+
+  #ensures that the library name is entered
+  validates :library_name, presence: true
 end
